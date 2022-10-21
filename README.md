a6d14bc2d98d6ac6e74b5fae1dbe01ea16f9c013
RubixDev <silas.groh@t-online.de>
2022-10-21 16:05:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
-# CodeMirror 6 language package template
+# Homescript Language Support for CodeMirror
 
-This is an example repository containing a minimal [CodeMirror](https://codemirror.net/6/) language support package. The idea is to clone it, rename it, and edit it to create support for a new language.
+This package implements [Homescript](https://github.com/smarthome-go/homescript)
+language support for the [CodeMirror](https://codemirror.net/6/) code editor.
 
-Things you'll need to do (see the [language support example](https://codemirror.net/6/examples/lang-package/) for a more detailed tutorial):
-
- * `git grep EXAMPLE` and replace all instances with your language name.
-
- * Rewrite the grammar in `src/syntax.grammar` to cover your language. See the [Lezer system guide](https://lezer.codemirror.net/docs/guide/#writing-a-grammar) for information on this file format.
-
- * Adjust the metadata in `src/index.ts` to work with your new grammar.
-
- * Adjust the grammar tests in `test/cases.txt`.
-
- * Build (`npm run prepare`) and test (`npm test`).
-
- * Rewrite this readme file.
-
- * Optionally add a license.
-
- * Publish. Put your package on npm under a name like `codemirror-lang-EXAMPLE`.
+This code is released under the [GPL-3.0-only license](./LICENSE).
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,57 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import {
+    delimitedIndent,
+    foldInside,
+    foldNodeProp,
+    indentNodeProp,
+    LanguageSupport,
+    LRLanguage,
+} from '@codemirror/language'
+import { styleTags, tags as t } from '@lezer/highlight'
+import { parser } from './syntax.grammar'
 
-export const EXAMPLELanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
+export const HomescriptLanguage = LRLanguage.define({
+    parser: parser.configure({
+        props: [
+            indentNodeProp.add({
+                Application: delimitedIndent({ closing: ')', align: false }),
+            }),
+            foldNodeProp.add({
+                Application: foldInside,
+            }),
+            styleTags({
+                'for while loop if else try catch return break continue': t.controlKeyword,
+                in: t.operatorKeyword,
+                'let fn': t.definitionKeyword,
+                'import from': t.moduleKeyword,
+                as: t.keyword,
+                Bool: t.bool,
+                null: t.null,
+                Type: t.typeName,
+                'VariableName/Ident': t.variableName,
+                'CallExpr/VariableName/Ident': t.function(t.variableName),
+                Property: t.propertyName,
+                'CallExpr/MemberExpr/Property': t.function(t.propertyName),
+                'FnExpr/Ident': t.function(t.variableName),
+                'Parameters/Ident': t.local(t.variableName),
+                Comment: t.lineComment,
+                Number: t.number,
+                String: t.string,
+                '+ - "*" "/" % "**"': t.arithmeticOperator,
+                '|| &&': t.logicOperator,
+                '< <= > >= "!=" ==': t.compareOperator,
+                '=': t.definitionOperator,
+                '( ) { }': t.bracket,
+                '. , ;': t.separator,
+                BuiltinFunc: t.standard(t.function(t.variableName)),
+                BuiltinVar: t.standard(t.variableName),
+            }),
+        ],
+    }),
+    languageData: {
+        commentTokens: { line: '#' },
+    },
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function Homescript() {
+    return new LanguageSupport(HomescriptLanguage)
 }
